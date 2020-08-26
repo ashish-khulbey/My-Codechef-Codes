4d0b8ca04746a77d6e7a0705ba16329de150f80c
Ashish Khulbey <65804342+ashish-khulbey@users.noreply.github.com>
2020-08-26 14:07:55 +0530

    Create HS08TEST.cpp

---
+++ HS08TEST.cpp
@@ -0,0 +1,17 @@
+Problem Link : https://www.codechef.com/problems/HS08TEST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	float y;
+	cin>>x>>y;
+	if ((x%5 == 0) && (x+0.5)<=y) {
+	    cout<<fixed<<setprecision(2)<<(y-x-0.5)<<endl;
+	}
+	else {
+	    cout<<fixed<<setprecision(2)<<(y)<<endl;
+	}
+	return 0;
+}
