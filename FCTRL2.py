0bfe6d74dec4a33e4d295e1deda6d7dc7331887c
Ashish Khulbey <65804342+ashish-khulbey@users.noreply.github.com>
2020-08-26 14:51:30 +0530

    Create FCTRL2.py

---
+++ FCTRL2.py
@@ -0,0 +1,10 @@
+# Problem Link : https://www.codechef.com/problems/FCTRL2
+
+t = int(input())
+while t>0:
+    n = int(input())
+    ans = 1
+    for i in range(1,n+1):
+        ans *= i
+    print(ans)
+    t=t-1
